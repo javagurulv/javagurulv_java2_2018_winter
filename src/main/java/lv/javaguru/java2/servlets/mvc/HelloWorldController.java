4b0b346ecfd47f47f70f5010b64574546b816313
Viktor Savonin <viktor.savonin@meawallet.com>
2018-05-10 10:28:59 +0300

    Lesson 11 - Spring MVC intro

--- src/main/java/lv/javaguru/java2/servlets/mvc/HelloWorldController.java
+++ src/main/java/lv/javaguru/java2/servlets/mvc/HelloWorldController.java
@@ -2,30 +2,46 @@ package lv.javaguru.java2.servlets.mvc;
 
 import lv.javaguru.java2.businesslogic.addproduct.AddProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Logger;
 
-@Component
-public class HelloWorldController implements MVCController {
+@Controller
+public class HelloWorldController {
+
+    private static Logger logger = Logger.getLogger(HelloWorldController.class.getName());
 
     @Autowired
     private AddProductService addProductService;
 
-    @Override
-    public MVCModel processGet(HttpServletRequest request) {
+    @RequestMapping(value = "hello/{id}", method = {RequestMethod.GET})
+    public ModelAndView processGet(HttpServletRequest request,
+                                   @PathVariable Long id) {
+
+        logger.info("ID = " + id);
 
         String productTitle = request.getParameter("title");
         String productDescription = request.getParameter("description");
 
         addProductService.addProduct(productTitle, productDescription);
 
-        return new MVCModel("/helloWorld.jsp", "Hello from MVC!");
+        return new ModelAndView("helloWorld", "model", "Hello from MVC!");
     }
 
-    @Override
-    public MVCModel processPost(HttpServletRequest request) {
-        throw new UnsupportedOperationException();
+    @RequestMapping(value = "hello", method = {RequestMethod.GET})
+    public ModelAndView process(HttpServletRequest request) {
+
+        String productTitle = request.getParameter("title");
+        String productDescription = request.getParameter("description");
+
+        addProductService.addProduct(productTitle, productDescription);
+
+        return new ModelAndView("helloWorld", "model", "Hello from MVC!");
     }
 
 }
